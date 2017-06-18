d92c9610539deae0745a63ea6c46f330756af32e
kajalRai <kajalrai.up@gmail.com>
2017-06-18 21:57:23 +0530

    Update imageviewer.py

--- imageviewer.py
+++ imageviewer.py
@@ -3,7 +3,7 @@
 import os,sys,commands ,socket,time
 f=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 os.system('ssh root@192.168.1.117 -X eog')
-execfile('saas.py')
+ 
 print "Do you want other services ? yes/no"
 ch1=raw_input()
 if ch1=="Yes" or ch1=="yes"  :
